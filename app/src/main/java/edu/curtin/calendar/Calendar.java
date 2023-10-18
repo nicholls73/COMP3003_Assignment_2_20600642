c898309bf7f4d035e4689452af676fac870c600f
James <jamnich73@gmail.com>
2023-10-19 00:06:52 +0800

    Started to get the calendar to display.
    Started to get the calendar to display.

--- app/src/main/java/edu/curtin/calendar/Calendar.java
+++ app/src/main/java/edu/curtin/calendar/Calendar.java
@@ -1,7 +1,28 @@
 package edu.curtin.calendar;
 
+import java.time.*;
+import java.util.*;
+
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LocalDateTime now = LocalDateTime.now();
+
+        List<List<String>> data = new ArrayList<List<String>>();
+        for (int i = 0; i < 24; i++) {
+            data.add(Arrays.asList("one", "two", "three", "four", "five", "six", "seven"));
+        }
+
+        List<String> rowHeadings = new ArrayList<String>();
+        for (int i = 0; i < 24; i++) {
+            rowHeadings.add("hour " + (i + 1));
+        }
+        String[] colHeadings = { now.getDayOfWeek().toString(), now.getDayOfWeek().plus(1).toString(),
+                now.getDayOfWeek().plus(2).toString(), now.getDayOfWeek().plus(3).toString(),
+                now.getDayOfWeek().plus(4).toString(), now.getDayOfWeek().plus(5).toString(),
+                now.getDayOfWeek().plus(6).toString() };
+
+        TerminalGrid terminalGrid = TerminalGrid.create();
+
+        terminalGrid.print(data, rowHeadings, List.of(colHeadings));
     }
 }
